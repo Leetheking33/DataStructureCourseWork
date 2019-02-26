c232f96b719d72d0399e973f5c26a3282c78e91f
Leetheking33 <henryleroy14@yahoo.com>
2019-02-25 20:21:58 -0500

    lab12

--- lab12/src/sllist.cpp
+++ lab12/src/sllist.cpp
@@ -1,7 +1,9 @@
 #ifdef SLLIST_HPP
+using namespace std;
 
 template<class T>
 SLList<T>::SLList() {
+	mHead = NULL;
 }
 
 /* Do a deep copy of sll into the this.
@@ -9,23 +11,95 @@ SLList<T>::SLList() {
  */
 template<class T>
 SLList<T>::SLList(const SLList<T> &sll) {
+	sNode<T> *rover = sll.mHead;
+	sNode<T> *newRov = NULL;
+	
+	if(sll.mHead == NULL){
+		mHead = NULL;
+	}
+	else{
+
+		for(int i=0; i < sll.mSize; i++){
+			if(i == 0){
+				sNode<T> *node = new sNode<T>();
+                		node->mData = rover->mData;
+				node->mNext = NULL;
+				mHead = node;
+				rover = rover->mNext;
+				newRov = mHead;
+				mSize +=1;
+			}
+			else{
+			sNode<T> *node = new sNode<T>();
+			node->mData = rover->mData;
+			node->mNext = NULL;
+			newRov->mNext = node;
+			rover = rover->mNext;
+			newRov = newRov->mNext;
+			mSize +=1;	
+			}	
+		}
+	}
 }
 
 template<class T>
+void SLList<T>::destroyList(){
+        sNode<T> *rover = mHead;
+        sNode<T> *prev = mHead;
+
+        while(rover != NULL){
+                prev = rover;
+                rover = rover->mNext;
+                delete prev;
+        }
+        mHead = NULL;
+        delete rover;
+	mSize = 0;
+}
+
+
+template<class T>
 SLList<T>::~SLList() {
+	destroyList();
 }
 
 template<class T>
 bool SLList<T>::append(const T &val) {
+	sNode<T> *rover = mHead;
+	sNode<T> *trailer;
+	sNode<T> *node = new sNode<T>();
+	if(mHead == NULL){
+		node->mData = val;
+		node->mNext = mHead;
+		mHead = node;
+		mSize = 1;	
+		return false;
+	}
+	while(rover != NULL){
+		trailer = rover;
+		rover = rover->mNext;
+	}
+	mSize += 1;
+	node->mData = val;
+	node->mNext = NULL;
+	trailer->mNext = node;
+
 }
 
 template<class T>
 void SLList<T>::print() const {
+	sNode<T> *rover = mHead;
+	while(rover != NULL){
+		cout << rover->mData << ", ";
+		rover = rover->mNext;		
+	}
+	cout << endl;
 }
 
 /* Return the current length of the Singly Linked List */
 template<class T>
 int SLList<T>::getLength() const {
+	return mSize;
 }
 
 /* Insert val at position pos.
@@ -34,7 +108,52 @@ int SLList<T>::getLength() const {
  */
 template<class T>
 bool SLList<T>::insert(const int pos, const T &val) {
-    return true;
+	sNode<T> *rover = mHead;
+        sNode<T> *prev = NULL;
+
+
+
+        if(pos == 0 && mSize == 0){
+                sNode<T> *node = new sNode<T>();
+                node->mData = val;
+                node->mNext = NULL;
+                mHead = node;
+                mSize += 1;
+                return true;
+        }
+
+        if(pos == 0 && mSize == 1){
+                sNode<T> *node = new sNode<T>();
+                node->mData = val;
+                node->mNext = rover;
+                mHead = node;
+                mSize += 1;
+                return true;
+        }
+
+        if(pos == 0){
+                sNode<T> *node = new sNode<T>();
+                node->mData = val;
+                node->mNext = rover;
+                mHead = node;
+                mSize += 1;
+                return true;
+        }
+
+        for(int i=0; i < pos; i++){
+                if(rover->mNext == NULL){
+                        return false;
+                }
+
+                prev = rover;
+                rover = rover->mNext;
+        }
+        sNode<T> *node = new sNode<T>();
+        node->mData = val;
+        node->mNext = rover;
+        prev->mNext = node;
+        mSize += 1;
+        return true;
 }
 
 /* Remove the first instance of val
@@ -43,15 +162,312 @@ bool SLList<T>::insert(const int pos, const T &val) {
  */
 template<class T>
 bool SLList<T>::remove(const T &val) {
-    return true;
+	sNode<T> *rover = mHead;
+	sNode<T> *prev = NULL;
+
+	if(mHead == NULL){
+		return false;
+	}
+
+	if(rover->mData == val){
+			mHead = rover->mNext;
+                        delete rover;
+			mSize -=1;
+                        return true;
+	}
+
+	while(rover != NULL){
+		if(rover->mData == val){
+			prev->mNext = rover->mNext;
+			delete rover;
+			mSize -= 1;
+			return true;
+		}
+		prev = rover;
+		rover = rover->mNext;
+	}
+	return false;
 }
 
 template<class T>
-T& SLList<T>::operator[](const int pos) const {
+T& SLList<T>::operator[](const int pos) {
+	sNode<T> *rover = mHead;
+	for(int i=0; i < mSize; i++){
+		if(i == pos){
+			return rover->mData;
+		}
+		rover = rover->mNext;
+	}
+	return rover->mData;
 }
 
 template<class T>
 bool SLList<T>::operator==(const SLList<T> &list) const {
+	sNode<T> *rover = mHead;
+	sNode<T> *lover = list.mHead;
+	
+	if(mSize != list.mSize){
+		return false;
+	}
+	
+	if(mHead == NULL && list.mHead == NULL){
+		return true;
+	}
+
+	while(rover != NULL){
+		if(rover->mData == lover->mData){
+			rover = rover->mNext;
+			lover = lover->mNext;
+			continue;
+		}
+		return false;
+	}
+	return true;
+}
+
+//This is  DLL fucnctions
+template<class T>
+DLList<T>::DLList() {
+        mHead = NULL;
+}
+
+/* Do a deep copy of sll into the this.
+ * Note: This one uses a reference to a Singly Linked List!
+ */
+template<class T>
+DLList<T>::DLList(const DLList<T> &dll) {
+        dNode<T> *rover = dll.mHead;
+        dNode<T> *newRov = NULL;
+
+        if(dll.mHead == NULL){
+                mHead = NULL;
+        }
+        else{
+
+                for(int i=0; i < dll.mSize; i++){
+                        if(i == 0){
+                                dNode<T> *node = new dNode<T>();
+                                node->mData = rover->mData;
+                                node->mNext = NULL;
+				node->mPrev = NULL;
+				mHead = node;
+                                rover = rover->mNext;
+                                newRov = mHead;
+                                mSize +=1;
+                        }
+                        else{
+                        dNode<T> *node = new dNode<T>();
+                        node->mData = rover->mData;
+                        node->mNext = NULL;
+			node->mPrev = newRov;
+                        newRov->mNext = node;
+                        rover = rover->mNext;
+                        newRov = newRov->mNext;
+                        mSize +=1;
+                        }
+                }
+        }
+}
+
+template<class T>
+void DLList<T>::destroyList(){
+	dNode<T> *rover = mHead;
+	dNode<T> *prev = mHead;
+
+	while(rover != NULL){
+		prev = rover;
+		rover = rover->mNext;
+		delete prev;
+	}
+	mHead = NULL;
+	delete rover;
+	mSize = 0;
+}
+
+template<class T>
+DLList<T>::~DLList() {
+	destroyList();
+}
+
+template<class T>
+bool DLList<T>::append(const T &val) {
+        dNode<T> *rover = mHead;
+	dNode<T> *node = new dNode<T>();
+        if(mHead == NULL){
+                node->mData = val;
+                node->mNext = NULL;
+		node->mPrev = NULL;
+                mHead = node;
+                mSize = 1;
+                return true;
+        }
+        while(rover->mNext != NULL){
+                rover = rover->mNext;
+        }
+        mSize += 1;
+        node->mData = val;
+        node->mNext = NULL;
+	node->mPrev = rover;
+	rover->mNext =node;
+}
+
+template<class T>
+void DLList<T>::print() const {
+        dNode<T> *rover = mHead;
+        while(rover != NULL){
+                cout << rover->mData << ", ";
+                rover = rover->mNext;
+        }
+	cout << endl;
+}
+
+/* Return the current length of the Singly Linked List */
+template<class T>
+int DLList<T>::getLength() const {
+        return mSize;
+}
+
+/* Insert val at position pos.
+ * Return true if successful (it can be placed.)
+ * Otherwise return false.
+ */
+template<class T>
+bool DLList<T>::insert(const int pos, const T &val) {
+        dNode<T> *rover = mHead;
+        dNode<T> *prev = NULL;
+
+
+	
+	if(pos == 0 && mSize == 0){
+		dNode<T> *node = new dNode<T>();
+		node->mData = val;
+		node->mPrev = NULL;
+		node->mNext = NULL;
+		mHead = node;
+		mSize += 1;
+		return true;
+	}
+
+	if(pos == 0 && mSize == 1){
+		dNode<T> *node = new dNode<T>();
+		node->mData = val;
+		node->mNext = rover;
+		node->mPrev = NULL;
+		rover->mPrev = node;
+		mHead = node;
+		mSize += 1;
+		return true;
+	}
+
+	if(pos == 0){
+		dNode<T> *node = new dNode<T>();
+		node->mData = val;
+		node->mNext = rover;
+		node->mPrev = NULL;
+                rover->mPrev = node;
+		mHead = node;
+                mSize += 1;
+                return true;
+	}
+
+	for(int i=0; i < pos; i++){
+                if(rover->mNext == NULL){
+                        return false;
+                }
+
+                prev = rover;
+                rover = rover->mNext;
+        }
+	dNode<T> *node = new dNode<T>();
+	node->mData = val;
+        node->mNext = rover;
+        prev->mNext = node;
+	node->mPrev = prev;
+        mSize += 1;
+        return true;
+}
+
+/* Remove the first instance of val
+ * Return true if found and removed.
+ * Otherwise return false.
+ */
+template<class T>
+bool DLList<T>::remove(const T &val) {
+        dNode<T> *rover = mHead;
+        dNode<T> *prev = NULL;
+	dNode<T> *inFront = NULL;
+
+	if(mHead == NULL){
+                return false;
+        }
+
+        if(rover->mData == val){
+                        mHead = rover->mNext;
+			inFront = rover->mNext;
+			inFront->mPrev = NULL;
+                        delete rover;
+                        mSize -=1;
+                        return true;
+        }
+
+        while(rover != NULL){
+                if(rover->mData == val){
+			if(rover->mNext == NULL){
+				prev->mNext = NULL;
+				delete rover;
+				mSize -= 1;
+				return true;
+			}
+			else{
+				inFront = rover->mNext;
+				inFront->mPrev = prev;
+                		prev->mNext = rover->mNext;
+                		delete rover;
+                        	mSize -= 1;
+                        	return true;
+			}
+                }
+
+                prev = rover;
+                rover = rover->mNext;
+        }
+        return false;
+}
+
+template<class T>
+T& DLList<T>::operator[](const int pos) {
+        dNode<T> *rover = mHead;
+        for(int i=0; i < mSize; i++){
+                if(i == pos){
+                        return rover->mData;
+                }
+                rover = rover->mNext;
+        }
+        return rover->mData;
+}
+
+template<class T>
+bool DLList<T>::operator==(const DLList<T> &list) const {
+        dNode<T> *rover = mHead;
+        dNode<T> *lover = list.mHead;
+
+        if(mSize != list.mSize){
+                return false;
+        }
+
+        if(mHead == NULL && list.mHead == NULL){
+                return true;
+        }
+
+        while(rover != NULL){
+                if(rover->mData == lover->mData){
+                        rover = rover->mNext;
+                        lover = lover->mNext;
+                        continue;
+                }
+                return false;
+        }
+        return true;
 }
 
 #endif
--- lab12/src/sllist.hpp
+++ lab12/src/sllist.hpp
@@ -1,19 +1,30 @@
 #ifndef SLLIST_HPP
 #define SLLIST_HPP
 
+/* DLL Node */
+template<class T>
+class dNode {
+	public:
+	T mData;
+	dNode *mNext;
+	dNode *mPrev;
+};
+
+/* SLL Node */
+template<class T>
+class sNode {
+	public:
+	T mData;
+	sNode *mNext;
+};
+
 /* SLL = Singly Linked List */
 template<class T>
 class SLList {
     private:
         /* Class exercise to fill in. */
-        class Node {
-            public:
-                T mData;
-                Node *mNext;
-        };
-
-        Node *mHead, *mTail;
-        int mSize;
+	sNode<T> *mHead;
+	int mSize =0;	
     public:
 
         /* Empty constructor shall create an empty Linked List! */
@@ -23,6 +34,8 @@ class SLList {
          * Note: This one uses a reference to a Singly Linked List!
          */
         SLList(const SLList<T> &sll);
+	
+	void destroyList(); 
 
         /* Deconstructor shall free up memory */
         ~SLList();
@@ -49,19 +62,67 @@ class SLList {
         bool remove(const T &val);
 
         /* Retrieves the element at position pos */
-        T& operator[](const int pos) const;
+        T& operator[](const int pos);
 
         /* Returns if the two lists contain the same elements in the
          * same order.*/
         bool operator==(const SLList<T> &list) const;
-
-        /* Sort the linked list.  You may use any sort algorithm you wish */
-        void sort();
 };
 
 /* Since SLList is templated, we include the .cpp
  * Templated classes are not generated until utilized (or explicitly declared.)
  */
+
+/* DLL = Doubly Linked List */
+template<class T>
+class DLList {
+    private:
+        /* Class exercise to fill in. */
+        dNode<T> *mHead;
+        int mSize =0;
+    public:
+
+        /* Empty constructor shall create an empty Linked List! */
+        DLList();
+
+        /* Do a deep copy of dll into the this.
+         * Note: This one uses a reference to a Doubly Linked List!
+         */
+        DLList(const DLList<T> &dll);
+	
+	void destroyList();	
+
+        /* Deconstructor shall free up memory */
+        ~DLList();
+
+        /* Return the current length of the Doubly Linked List */
+        int getLength() const;
+
+        /* Insert at the end of the list.*/
+        bool append(const T &val);
+
+        /* Insert val at position pos.
+         * Return true if successful (it can be placed.)
+         * Otherwise return false.
+         */
+        bool insert(const int pos, const T &val);
+	
+	/* Print out the Doubly Linked List */
+        void print() const;
+
+        /* Remove the first instance of val
+         * Return true if found and removed.
+         * Otherwise return false.
+         */
+        bool remove(const T &val);
+
+        /* Retrieves the element at position pos */
+        T& operator[](const int pos);
+
+        /* Returns if the two lists contain the same elements in the
+         * same order.*/
+        bool operator==(const DLList<T> &list) const;
+};
 
 #include "sllist.cpp"
 
