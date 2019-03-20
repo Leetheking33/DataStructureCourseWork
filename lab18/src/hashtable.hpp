c33b616a29bbbbd6be007920b4434d8ab24d0a1a
Dr. West <pwest@cs.univ.edu>
2019-03-20 06:56:54 -0400

    lab18

---
+++ lab18/src/hashtable.hpp
@@ -0,0 +1,44 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <string>
+#include <vector>
+#include <list>
+
+/* HashTable via chaining */
+template<class K, class V>
+class HashTable {
+    private:
+    public:
+        /* Initialize the Hash Table with size size. */
+        HashTable(const int size);
+
+        /* Deconstructor shall free up memory */
+        ~HashTable();
+
+        /* Map key -> val.
+         * Return true if sucessful (it is unique.)
+         * Otheriwise return false.
+         */
+        bool insert(const K &key, const V &val);
+
+        /* Print out the HashTable */
+        void print() const;
+
+        /* Remove the val associated with key.
+         * Return true if found and removed.
+         * Otherwise return false.
+         */
+        bool remove(const K &key);
+
+        /* Retrieves the V val that key maps to. */
+        V& operator[](const K &key);
+
+};
+
+int hashcode(int key);
+int hashcode(const std::string &key);
+
+#include "hashtable.cpp"
+
+#endif
