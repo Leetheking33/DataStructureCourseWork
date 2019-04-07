958f892848b46d459aa4deae349122f8cf6afc3a
Leetheking33 <henryleroy14@yahoo.com>
2019-04-07 13:01:11 -0400

    lab20

--- lab20/src/main.cpp
+++ lab20/src/main.cpp
@@ -2,4 +2,20 @@
 
 int main(int argc, char *argv[]) {
     PriorityQueue<int> pq;
-}
+    pq.push(10);
+    pq.push(20);
+    pq.push(56);
+    pq.push(-432);
+    pq.push(93);
+    pq.push(88);
+    pq.push(-6);
+    pq.push(5);
+    pq.print();
+    cout << pq.pop() << endl;
+    cout << pq.pop() << endl;
+    cout << pq.pop() << endl;
+    cout << pq.pop() << endl;
+    cout << pq.pop() << endl;
+    cout << pq.pop() << endl;
+    pq.print();
+}	
