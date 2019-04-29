8038a114fdd9eb49a267713406c18e2580bf4015
Leetheking33 <31420198+Leetheking33@users.noreply.github.com>
2019-04-28 23:52:19 -0400

    Add files via upload

--- lab24/src/quadtree.hpp
+++ lab24/src/quadtree.hpp
@@ -9,6 +9,7 @@
 
 class Quad {
     private:
+    int numOfPoints;
     Quad *topLeftQuad, *topRightQuad, *bottomLeftQuad, *bottomRightQuad;
     float pointX, pointY;
     bool hasPoint;
@@ -18,6 +19,7 @@ class Quad {
             topLeftQuad = topRightQuad = bottomLeftQuad = bottomRightQuad = NULL;
             hasPoint = false;
             sx = startx; sy = starty; ex = endx; ey = endy;
+            numOfPoints = 0;
         }
 
     bool getPoint(float &x, float &y){
@@ -28,10 +30,10 @@ class Quad {
         y = pointY;
         return true;
     }
-
+    bool hasAPoint(){return hasPoint;}
     void setPoint(float x, float y) { pointX = x; pointY = y; hasPoint = true; }
-    bool isLeaf() { return topRightQuad == NULL && topLeftQuad == NULL 
-        && bottomRightQuad == NULL && bottomLeftQuad == NULL;}
+    bool isLeaf() { return (topRightQuad == NULL && topLeftQuad == NULL 
+        && bottomRightQuad == NULL && bottomLeftQuad == NULL);}
     float getSX() { return sx; }
     float getSY() { return sy; }
     float getEX() { return ex; }
@@ -48,7 +50,11 @@ class Quad {
         bottomRightQuad = new Quad(midx,midy,ex,ey);
         bottomLeftQuad = new Quad(sx,midy,midx,ey);
     }
-    void clearPoint() { hasPoint = false; }
+    void clearPoint() { hasPoint = false;}
+    void decNumOfPoints() {numOfPoints--;}
+    void incNumOfPoints() {numOfPoints++;}
+    int getNumOfPoints() {return numOfPoints;}
+
 
 };
 
@@ -60,8 +66,12 @@ class QuadTree {
         Quad* root;
         float maxWidth;
         float maxHeight;
-        bool add(Quad *node, float x, float y);
+
+        bool containsPoint(Quad *node, float x, float y);
+
     public:
+
+        int numOfPoints(){return root->getNumOfPoints();}
         /* Initialize an empty quadtree. */
         QuadTree(float width, float height);
 
@@ -69,23 +79,28 @@ class QuadTree {
 
         /* add a point to the quadtree. */
         bool add(float x, float y);
+        bool add(Quad *node, float x, float y);
 
         /* remove a point from the QuadTree.
          * Remember to removing empty QuadTree nodes, or your tree will 
          * use up too much memory when doing the add/remove test!
          */
         bool remove(float x, float y);
+        bool remove(Quad *node, float x, float y);
 
         /* returns if the quad tree has a point (x, y) */
         bool contains(float x, float y);
+
 
         /* return the number of points in the box (sx, sy) -> (ex, ey) 
          * You may assume that sx < ex and sy < ey!
          */
         int countInRange(float sx, float sy, float ex, float ey);
+        int countInRange(Quad *node, float sx, float sy, float ex, float ey);
 
 
         void print();
+        void print(Quad* node);
 };
 
 #include "quadtree.cpp"
